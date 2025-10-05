4bc9ef1a8a3281a165aebdf931417bc14869634d
unknown <harshkumar34.fin@gmail.com>
2025-10-05 22:27:55 +0530

    Rate Limmiter added using Arcjet

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,32 @@
+import arcjet from '@arcjet/node'
+import aj from '../lib/arcjet'
+import {isSpoofedBot} from "@arcjet/inspect"
+
+export const arcjetProtection = async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req)
+
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) {
+                return res.status(429).json({message: "Rate Limit Exceeded. Please Try Again Later."})
+            }
+        
+            else if(decision.reason.isBot){
+                return res.status(429).json({message: "Bot Access Denied."})
+            }
+            else{
+                return res.status(429).json({message: "Access Denied By Security Policy"})
+            }
+        }
+        
+        if(decision.results.some(isSpoofedBot)) {
+            return res.status(403).json({
+                error: "Spoofed Bot Detected.",
+                message: "Malicious bot activity detected"
+            })
+        }
+    } catch (error) {
+        console.log("Arcjet Protection Error", error)
+        next();
+    }
+}
