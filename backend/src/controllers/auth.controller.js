c32b8359719907c56c0349dbbe6d25beb19bad9b
unknown <harshkumar34.fin@gmail.com>
2025-10-05 22:01:59 +0530

    update profile feature added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs"
 import { generateToken } from "../lib/utils.js"
 import { sendWelcomeEmail } from "../emails/emailHandler.js";
 import dotenv from "dotenv"
+import cloudinary from "../lib/cloudinary.js";
 dotenv.config();
 
 export const signup = async (req, res) => {
@@ -62,6 +63,10 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const {email, password} = req.body;
 
+    if(!email || !password){
+        return res.status(400).json({message: "Email and Password are required"})
+    }
+
     try {
         const user = await User.findOne({email})
         if(!user) return res.status(400).json({message: "Invalid Credentials"})
@@ -86,4 +91,26 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
     res.cookie("jwt", "", {maxAge: 0})
     res.status(200).json({message: "Logged out successfully"})
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        if(!profilePic) return res.status(400).json({message: "Profile picture is required."})
+
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            {profilePic: uploadResponse.secure_url},
+            {new: true}
+        );
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("error in update profile", error)
+        res.status(500).json({message: "Internal Server Error."});
+    }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import path from "path"
+import cookieParser from 'cookie-parser'
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +12,7 @@ dotenv.config()
 const __dirname = path.resolve()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
