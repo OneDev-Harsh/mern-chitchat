71a3fc2cad912d744f298fe84698bf6350acf0ce
unknown <harshkumar34.fin@gmail.com>
2025-10-08 15:45:21 +0530

    Payload limit increased

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,8 @@ const app = express()
 dotenv.config()
 const __dirname = path.resolve()
 
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use(cookieParser())
 
